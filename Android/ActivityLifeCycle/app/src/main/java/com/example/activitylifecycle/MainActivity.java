54614743135d40d2e8520dd0949643ff08374f5b
DreamOf <xtns13@gmail.com>
2020-11-03 09:15:52 +0300

    Added Android(ActivityLifeCycle)

---
+++ Android/ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewLifeCycle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewLifeCycle = findViewById(R.id.textViewLifeCycle);
+        Log.d("LifeCycle: ", "onCreate()");
+        textViewLifeCycle.append("onCreate()" + "\n");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle: ", "onStart()");
+        textViewLifeCycle.append("onStart()" + "\n");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCycle: ", "onResume()");
+        textViewLifeCycle.append("onResume()" + "\n");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle: ", "onPause()");
+        textViewLifeCycle.append("onPause()" + "\n");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCycle: ", "onStop()");
+        textViewLifeCycle.append("onStop()" + "\n");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle: ", "onDestroy()");
+        textViewLifeCycle.append("onDestroy()" + "\n");
+    }
+}
